5155c689c0183cbe02c55a1077cf5ef416841b89
Mathesh Yogeswaran <matheshyogeswaran@gmail.com>
2026-05-05 09:58:37 +0100

    Initial Commit

---
+++ BookVault/BookVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookVaultApp.swift
+//  BookVault
+//
+//  Created by Mathesh Yogeswaran on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
